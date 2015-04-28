d81378a122944214f7abfd198bc68517626aa3ae
Wei Zhao <wei.zhao@tamu.edu>
2015-04-28 15:16:46 -0500

    updated

---
+++ step-20/CMakeFiles/step-20.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/step-20.dir/step-20.cc.o"
+  "step-20.pdb"
+  "step-20"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/step-20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
